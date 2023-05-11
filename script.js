aabda26fa4d7c86b459764fde10002f0a992a588
Kevin Foster <42094092+kevfoste@users.noreply.github.com>
2023-05-11 16:42:11 -0400

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * Add .gitignore template file to repository
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
